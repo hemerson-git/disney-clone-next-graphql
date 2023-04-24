73aa3c718012f886e60c268ad701a9dbd46824b3
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-04-24 02:40:21 -0300

    INITIAL commit

---
+++ src/components/Header/header.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Header } from '.';
+
+describe('<Header/>', () => {
+  it('should be able to sum', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
